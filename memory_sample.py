ce009ce9782e5ce93db0266c9963d3e08acb70c5
kskesakai <kske.sakai@gmail.com>
2023-08-18 19:05:42 +0900

    add memory_sapmle.py

---
+++ memory_sample.py
@@ -0,0 +1,9 @@
+import WaterfallChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import LLMChain
+from enum import Enum
